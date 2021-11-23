7c2cb95483c0d0278a018bf51cd429f503e8c132
vitor rosa <vitorchx@outlook.com>
2021-11-22 21:07:48 -0300

    basic

---
+++ src/programJava1/programJava.java
@@ -0,0 +1,14 @@
+package programJava1;
+
+ public class programJava {
+ public static void main(String[] args) {
+	 
+	 Cat cat = new Cat();
+	  System.out.println(cat);
+	 
+	 /*int a = 2;
+	 int b = 3;
+	  System.out.println("Heloo World! " + (a+b));*/
+	
+ }
+}
